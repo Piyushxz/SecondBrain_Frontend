7a95407329706623b17abdd7ba66f5e2eccd70aa
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-01-26 13:19:45 +0530

    related content

--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,10 +1,12 @@
 import { useRecoilValue } from "recoil"
 import { queryResponse } from "../atoms"
+import { SearchResultCard } from "./ui/SearchResultCard"
 export const SearchResult = ()=>{
 
     const response = useRecoilValue(queryResponse)
 
     console.log(response?.data.answer)
+    console.log(response?.data.sources[0])
 
     return(
         <div className="grid grid-cols-12 md:w-[70vw] w-[80vw] mt-4 gap-4">
@@ -14,11 +16,7 @@ export const SearchResult = ()=>{
                 <p className="">{response?.data.answer}</p>
             </div>
         </div>
-        <div className="md:col-span-3 col-span-12 ">
-            <div className="px-4 h-24 border border-white rounded-lg">
-
-            </div>
-        </div>
+        <SearchResultCard variant={response?.data.sources[0].type} title={response?.data.sources[0].content}/>
     </div>
     )
 }
---
+++ src/components/ui/SearchResultCard.tsx
@@ -0,0 +1,30 @@
+import { DocumentIcon } from "../../icons/DocumentIcon"
+import { YoutubeIcon } from "../../icons/YoutubeIcon"
+import { TwitterIcon } from "../../icons/TwitterIcon"
+
+interface cardProps{
+    variant:"tweet" | "youtube" | "link",
+    title:string
+}
+const iconStyles={
+    link:  <DocumentIcon variant="lg"/>,
+    youtube:<YoutubeIcon variant="lg"/>,
+    tweet:<TwitterIcon variant="lg"/>
+
+}
+export const SearchResultCard = (props:cardProps) =>{
+    return(
+        
+        <div className="md:col-span-3 col-span-12 ">
+                <h1 className="font-satoshi tracking-tighter font-black text-white text-lg pl-2">Related Content,</h1>
+
+        <div className="px-4 h-24 border border-white rounded-lg">
+            <div className="flex gap-2 p-2 ">
+                {iconStyles[props.variant]}
+                <h1 className="font-satoshi tracking-tighter font-black text-white text-lg pl-2">{props.title}</h1>
+
+            </div>
+        </div>
+    </div> 
+    )
+}
